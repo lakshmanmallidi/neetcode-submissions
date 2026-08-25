71039ae69d1f52904d04b8bc49b257b098ac1fbe
lakshman mallidi <lakshman.mallidi@gmail.com>
2026-08-25 23:12:29 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        freq_map = {}
+        for strg in strs:
+            freqs = [0] * 26
+            for char in strg:
+                freqs[ord(char) - ord('a')] += 1 
+            if tuple(freqs) in freq_map:
+                freq_map[tuple(freqs)].append(strg)
+            else:
+                freq_map[tuple(freqs)] = [strg]
+        group = []
+        for _, value in freq_map.items():
+            group.append(value)
+        return group
